e33d06607f20ee0383e42a84bec884b8a28f6423
SteshaVolkova <stepashechka5@gmail.com>
2025-03-08 18:13:45 +0300

    Шаг 5. Проверка авторизации и загрузка данных

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -5,7 +5,7 @@ export const routes = [
     path: "",
     name: "home",
     component: () => import("@/views/HomeView.vue"),
-    meta: { layout: "AppLayoutDefault" },
+    meta: { layout: "DefaultLayout" },
   },
   {
     path: "/login",
@@ -17,14 +17,22 @@ export const routes = [
     path: "/cart",
     name: "cart",
     component: () => import("@/views/CartView.vue"),
-    meta: { layout: "AppLayoutDefault" },
+    meta: {
+      layout: "DefaultLayout",
+    },
+  },
+  {
+    path: "/success",
+    name: "success",
+    component: () => import("@/views/SuccessView.vue"),
+    meta: { layout: "SimpleLayout" },
   },
   {
     path: "/user",
     name: "user",
     component: () => import("@/views/UserView.vue"),
     meta: {
-      layout: "AppLayoutDefault",
+      layout: "DefaultLayout",
       middlewares: [isLoggedIn],
     },
     children: [
@@ -39,11 +47,5 @@ export const routes = [
         component: () => import("@/views/ProfileView.vue"),
       },
     ],
-  },
-  {
-    path: "/success",
-    name: "success",
-    component: () => import("@/views/SuccessView.vue"),
-    meta: { layout: "SimpleLayout" },
   },
 ];
